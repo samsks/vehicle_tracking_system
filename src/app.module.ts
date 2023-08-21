66a5579da43aa9981b81cc8bb634adaa0e0dea01
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-21 20:54:54 +0000

    feat: :hammer: create route resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })
