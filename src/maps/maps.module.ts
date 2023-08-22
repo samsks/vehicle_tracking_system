5530397a3c76ed03e71142731f41abea0b15c6d0
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 03:49:23 +0000

    feat: :hammer: insert directions route

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
 import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
 
 @Module({
-  controllers: [PlacesController],
+  controllers: [PlacesController, DirectionsController],
   providers: [
     PlacesService,
     {
       provide: GoogleMapsClient,
       useValue: new GoogleMapsClient(),
     },
+    DirectionsService,
   ],
 })
 export class MapsModule {}
