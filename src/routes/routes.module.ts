e03d1a0a15fd22d1b3e1b8c5ad8f492b8f03d7b5
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 04:41:43 +0000

    feat: :sparkles: add routes-driver service

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
+import { RoutesDriverService } from './routes-driver/routes-driver.service';
 
 @Module({
   imports: [MapsModule],
   controllers: [RoutesController],
-  providers: [RoutesService],
+  providers: [RoutesService, RoutesDriverService],
 })
 export class RoutesModule {}
