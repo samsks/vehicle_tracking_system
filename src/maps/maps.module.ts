cedd502386aef6668a92bd5abad81aac7e30ed4b
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 03:27:38 +0000

    feat: :hammer: add complete route for maps/place GET

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlacesController } from './places/places.controller';
 import { PlacesService } from './places/places.service';
+import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
 
 @Module({
   controllers: [PlacesController],
-  providers: [PlacesService],
+  providers: [
+    PlacesService,
+    {
+      provide: GoogleMapsClient,
+      useValue: new GoogleMapsClient(),
+    },
+  ],
 })
 export class MapsModule {}
--- src/maps/places/places.controller.ts
+++ src/maps/places/places.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PlacesService } from './places.service';
 
 @Controller('places')
-export class PlacesController {}
+export class PlacesController {
+  constructor(private placeService: PlacesService) {}
+
+  @Get()
+  findPlace(@Query('text') text: string) {
+    return this.placeService.findPlace(text);
+  }
+}
