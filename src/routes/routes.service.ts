1bf51d9326e8f3241ed2b934d05455539c5064a6
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 02:06:36 +0000

    feat: :hammer: insert create and listAll for routes resources

--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class RoutesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createRouteDto: CreateRouteDto) {
-    return 'This action adds a new route';
+    return this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source: {
+          name: createRouteDto.source_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        destination: {
+          name: createRouteDto.destination_id,
+          location: {
+            lat: 0,
+            lng: 0,
+          },
+        },
+        distance: 0,
+        duration: 0,
+        directions: '{}',
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all routes`;
+    return this.prismaService.route.findMany();
   }
 
   findOne(id: number) {
