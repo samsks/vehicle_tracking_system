9675f4de7d7e52bc5c7ff6d0bab23bf67cb0d7b5
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 03:58:27 +0000

    feat: :white_check_mark: update GET routes values

--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -2,32 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
+import { DirectionsService } from 'src/maps/directions/directions.service';
 
 @Injectable()
 export class RoutesService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private directionsService: DirectionsService,
+  ) {}
 
-  create(createRouteDto: CreateRouteDto) {
+  async create(createRouteDto: CreateRouteDto) {
+    const { available_travel_modes, geocoded_waypoints, routes, request } =
+      await this.directionsService.getDirections(
+        createRouteDto.source_id,
+        createRouteDto.destination_id,
+      );
+    const legs = routes[0].legs[0];
     return this.prismaService.route.create({
       data: {
         name: createRouteDto.name,
         source: {
-          name: createRouteDto.source_id,
+          name: legs.start_address,
           location: {
-            lat: 0,
-            lng: 0,
+            lat: legs.start_location.lat,
+            lng: legs.start_location.lng,
           },
         },
         destination: {
-          name: createRouteDto.destination_id,
+          name: legs.end_address,
           location: {
-            lat: 0,
-            lng: 0,
+            lat: legs.end_location.lat,
+            lng: legs.end_location.lng,
           },
         },
-        distance: 0,
-        duration: 0,
-        directions: '{}',
+        distance: legs.distance.value,
+        duration: legs.duration.value,
+        directions: JSON.stringify({
+          available_travel_modes,
+          geocoded_waypoints,
+          routes,
+          request,
+        }),
       },
     });
   }
