30829a0fb68afdec94509982ea169cc43bdd093e
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-23 20:26:03 -0300

    fix(backend): :bug: fix: format response

--- nestjs/src/routes/routes.controller.ts
+++ nestjs/src/routes/routes.controller.ts
@@ -17,8 +17,9 @@ export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
   @Post()
-  create(@Body() createRouteDto: CreateRouteDto) {
-    return this.routesService.create(createRouteDto);
+  async create(@Body() createRouteDto: CreateRouteDto) {
+    const route = await this.routesService.create(createRouteDto);
+    return new RouteSerializer(route);
   }
 
   @Get()
