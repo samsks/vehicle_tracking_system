b4be34e1c4ebf17a5bd7b90cd6ca2bb840bc10aa
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-22 23:53:16 -0300

    feat(frontend): :sparkles: add base for new-route page

---
+++ nextjs/src/app/new-route/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import type {
+  DirectionsResponseData,
+  FindPlaceFromTextResponseData,
+} from "@googlemaps/google-maps-services-js";
+import { FormEvent } from "react";
+
+function NewRoutePage() {
+  async function handleSearchPlaces(event: FormEvent) {
+    event.preventDefault();
+
+    const source = (document.getElementById("source") as HTMLInputElement)
+      .value;
+    const destination = (
+      document.getElementById("destination") as HTMLInputElement
+    ).value;
+
+    const [sourceResponse, destinationResponse] = await Promise.all([
+      fetch(`http://localhost:3000/places?text=${source}`),
+      fetch(`http://localhost:3000/places?text=${destination}`),
+    ]);
+
+    const [sourcePlace, destinationPlace]: FindPlaceFromTextResponseData[] =
+      await Promise.all([sourceResponse.json(), destinationResponse.json()]);
+
+    if (sourcePlace.status !== "OK") {
+      console.error(sourcePlace);
+      alert("Não foi possível encontrar a origem");
+      return;
+    }
+
+    if (destinationPlace.status !== "OK") {
+      console.error(destinationPlace);
+      alert("Não foi possível encontrar o destino");
+      return;
+    }
+
+    const placeSourceId = sourcePlace.candidates[0].place_id;
+    const placeDestinationId = destinationPlace.candidates[0].place_id;
+
+    const directionsResponse = await fetch(
+      `http://localhost:3000/directions?originId=${placeSourceId}&destinationId=${placeDestinationId}`
+    );
+    const directionsData: DirectionsResponseData & { request: any } =
+      await directionsResponse.json();
+
+    console.log(directionsData);
+  }
+  return (
+    <div>
+      <h1>Nova rota</h1>
+      <form
+        onSubmit={handleSearchPlaces}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <div>
+          <input id="source" type="text" placeholder="origen" />
+        </div>
+        <div>
+          <input id="destination" type="text" placeholder="destino" />
+        </div>
+        <button type="submit">Pesquisar</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewRoutePage;
