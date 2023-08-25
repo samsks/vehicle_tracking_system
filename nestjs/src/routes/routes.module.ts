e8a41108a1f5f95a56accf1173ed1b3409a99143
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-25 11:23:34 -0300

    feat(backend): :sparkles: add: route gateway

--- nestjs/src/routes/routes.module.ts
+++ nestjs/src/routes/routes.module.ts
@@ -3,10 +3,11 @@ import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MapsModule } from 'src/maps/maps.module';
 import { RoutesDriverService } from './routes-driver/routes-driver.service';
+import { RoutesGateway } from './routes/routes.gateway';
 
 @Module({
   imports: [MapsModule],
   controllers: [RoutesController],
-  providers: [RoutesService, RoutesDriverService],
+  providers: [RoutesService, RoutesDriverService, RoutesGateway],
 })
 export class RoutesModule {}
