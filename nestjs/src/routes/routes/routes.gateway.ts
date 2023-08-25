77a9c0801ab2c18b55d70b0099e89cecaa424d7d
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-25 20:40:41 -0300

    feat(backend): :sparkles: add:  route gateway - websocket

--- nestjs/src/routes/routes/routes.gateway.ts
+++ nestjs/src/routes/routes/routes.gateway.ts
@@ -1,9 +1,22 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { RoutesDriverService } from '../routes-driver/routes-driver.service';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class RoutesGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  constructor(private routesDriverService: RoutesDriverService) {}
+
+  @SubscribeMessage('new-points')
+  async handleMessage(
+    client: Socket,
+    payload: { route_id: string; lat: number; lng: number },
+  ) {
+    await this.routesDriverService.createOrUpdate(payload);
+    client.broadcast.emit('admin-new-points', payload);
+    client.broadcast.emit(`new-points/${payload.route_id}`, payload);
   }
 }
