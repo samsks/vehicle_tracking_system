fa65d7a1abfaf3964c1ed2068dea8c8668d9ff84
SamSKS <samuel.meireles.15s@outlook.com>
2023-08-25 20:41:27 -0300

    feat(frontend): :sparkles: update: driver page

--- nextjs/src/app/driver/page.tsx
+++ nextjs/src/app/driver/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "../../hooks/useMap";
 import useSwr from "swr";
 import { fetcher } from "../../utils/http";
 import { Route } from "../../utils/model";
+import { socket } from "@/utils/socket-io";
 
 export function DriverPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
@@ -17,6 +18,13 @@ export function DriverPage() {
   } = useSwr<Route[]>("http://localhost:3000/routes", fetcher, {
     fallbackData: [],
   });
+
+  useEffect(() => {
+    socket.connect();
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   async function startRoute() {
     const routeId = (document.getElementById("route") as HTMLSelectElement)
@@ -42,9 +50,19 @@ export function DriverPage() {
     for (const step of steps) {
       await sleep(2000);
       map?.moveCar(routeId, step.start_location);
+      socket.emit("new-points", {
+        route_id: routeId,
+        lat: step.start_location.lat,
+        lng: step.start_location.lng,
+      });
 
       await sleep(2000);
       map?.moveCar(routeId, step.end_location);
+      socket.emit("new-points", {
+        route_id: routeId,
+        lat: step.end_location.lat,
+        lng: step.end_location.lng,
+      });
     }
   }
 
